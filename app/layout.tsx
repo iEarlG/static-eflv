cbf69ec0535f99eb396f103b78ed42dfbc6c0db3
Earl A. Villapaz <jasonburning16@gmail.com>
2024-01-29 19:10:16 +0800

    3: Header, Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+
 const pops = Poppins({ 
   subsets: ["latin"],
   weight: ["400", "600", "800", "900"]
@@ -21,7 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={pops.className}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
