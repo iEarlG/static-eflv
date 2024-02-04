eebf40b226526ac96ccaa13901f6d841a7a43554
Earl A. Villapaz <jasonburning16@gmail.com>
2024-02-04 16:21:34 +0800

    6.5: template

--- app/template.tsx
+++ app/template.tsx
@@ -5,8 +5,8 @@ import { motion } from "framer-motion";
 import { useScrollProgress } from "@/hooks/useScrollProgress";
 
 const variant = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
 };
 
 export default function Template({ 
@@ -15,14 +15,14 @@ export default function Template({
     const completion = useScrollProgress();
     return (
       <>
-        <motion.div 
+        <motion.main
           variants={variant} 
           initial="hidden" 
           animate="visible"
           transition={{type: "linear", delay: 0.2, duration: 0.4 }}
         >
           {children}
-        </motion.div>
+        </motion.main>
         <span 
           style={{transform: `translateY(${completion - 100}%)`}}
           className="fixed z-50 bg-primary w-1 top-0 right-0 bottom-0 transition-all duration-700">
