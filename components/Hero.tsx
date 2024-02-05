de8d6fadf5e4a93f42c75d34f9bd3164a391001b
Earl A. Villapaz <jasonburning16@gmail.com>
2024-02-05 19:07:44 +0800

    10: Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,26 @@
 
 import Link from "next/link";
-import { Download, Send } from "lucide-react";
+import { ArrowDown, Download, Send, ChevronDown} from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
 export const Hero = () => {
     return ( 
-        <div>
-            Hero
-        </div>
+        <section className="h-[84vh] py-12 xl:py-24 xl:pt-28">
+            <div className="container mx-auto">
+                <div className="flex justify-between gap-x-8">
+
+                    <div>
+                        Text
+                    </div>
+                    <div>
+                        Image
+                    </div>
+                </div>
+                <div className="absolute hidden md:flex left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+                    <ChevronDown className="text-primary h-7 w-7" />
+                </div>
+            </div>
+        </section>
     );
 }
