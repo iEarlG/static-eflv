4c1e61a86e77af4d81be0a24dd2f8ec6025b1ee0
Earl A. Villapaz <jasonburning16@gmail.com>
2024-02-07 14:59:55 +0800

    11: Optional CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,9 @@ const config = {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
+        tertiary: {
+          DEFAULT: "hsl(var(--tertiary))",
+        },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
