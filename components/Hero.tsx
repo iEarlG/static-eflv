3207309eb7a43afca167a674499cc40870ab1aa8
Earl A. Villapaz <jasonburning16@gmail.com>
2024-02-07 14:45:19 +0800

    10: Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,14 +12,21 @@ export const Hero = () => {
                     <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
                         <h1 className="text-sm uppercase font-semibold text-primary tracking-[4px] mb-2">Web Developer</h1>
                         <h2 className="h2-header">Hello, I am <br /> Earl A. <span className="text-primary">Villapaz</span></h2>
-                        <p className="text-muted-foreground mt-2 font-[400]">
+                        <p className="text-muted-foreground mb-4 font-[400]">
                             A brief description with insights into myself, my experience, <br /> and 
                             what I do professionally.
                         </p>
-                        <div>
+                        <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0">
                             <Link href="/contact">
-                                <Button>Contact me</Button>
+                                <Button className="gap-x-2">
+                                    Contact me
+                                    <Send size={18} />
+                                </Button>
                             </Link>
+                            <Button className="gap-x-2" variant="secondary">
+                                Resume
+                                <Download size={18} />
+                            </Button>
                         </div>
                     </div>
                     <div className="hidden xl:flex relative">
