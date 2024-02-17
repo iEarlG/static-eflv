7be08c975a76a36a177384b6b1655c83d2c37324
Earl A. Villapaz <jasonburning16@gmail.com>
2024-02-17 15:48:22 +0800

    16: Mobile Navigation

--- components/navigation/MobileNavbar.tsx
+++ components/navigation/MobileNavbar.tsx
@@ -1,8 +1,30 @@
+import { AlignJustify } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Navbar } from "@/components/navigation/Navbar";
+import { Socials } from "@/components/Socials";
+import { Logo } from "@/components/Logo";
 
 export const MobileNavbar = () => {
     return (
-        <div>
-            <h1>MobileNavbar</h1>
-        </div>
+        <Sheet>
+            <SheetTrigger asChild>
+                <AlignJustify />
+            </SheetTrigger>
+            <SheetContent>
+                <div className="flex flex-col items-center justify-between h-full py-8">
+                    <div className="flex flex-col items-center gap-y-32">
+                        <Logo />
+                        <Navbar 
+                            containerStyles="flex flex-col items-center gap-y-6 capitalize"
+                        />
+                    </div>
+                    <Socials 
+                        containerStyles="flex gap-x-4"
+                        iconStyles="text-[20px] leading-[24px]"
+                    />
+                </div>
+            </SheetContent>
+        </Sheet>
     );
 }
