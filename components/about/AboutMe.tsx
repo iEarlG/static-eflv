ce6b961ab98c0200e61ccedced44e5e0b438a51b
Earl A. Villapaz <jasonburning16@gmail.com>
2024-06-02 14:12:00 +0800

    18: AboutMe Qualification (noImplicitAny: false, 'temp') 1

--- components/about/AboutMe.tsx
+++ components/about/AboutMe.tsx
@@ -142,21 +142,25 @@ export const AboutMe = () => {
                                         <h2 className="text-3xl font-bold mb-8 text-center xl:text-left">My Journey</h2>
                                         <div>
                                             <div>
-                                                <div className="flex items-center gap-x-4 text-[18px]">
+                                                <div className="flex items-center gap-x-4 text-[18px] mb-2">
                                                     <Briefcase className="text-primary" />
                                                     <span className="capitalize font-medium">
                                                         {getData(qualifications, "Experience").title}
                                                     </span>
                                                 </div>
-                                                <div>
+                                                <div className="flex flex-col gap-y-8">
                                                     {getData(qualifications, "Experience").data.map((item, i) => {
                                                         const { company, position, year } = item;
                                                         return (
-                                                            <div key={i}>
+                                                            <div key={i} className="flex gap-x-8 group">
+                                                                <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                                                    <div className="h-[11px] w-[11px] rounded-full bg-primary absolute -left-[5px] 
+                                                                    group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                                                </div>
                                                                 <div>
-                                                                    <div>{company}</div>
-                                                                    <div>{position}</div>
-                                                                    <div>{year}</div>
+                                                                    <div className="font-semibold text-lg leading-none mb-2">{company}</div>
+                                                                    <div className="text-lg leading-none text-muted-foreground mb-4">{position}</div>
+                                                                    <div className="text-base font-medium">{year}</div>
                                                                 </div>
                                                             </div>
                                                         )
